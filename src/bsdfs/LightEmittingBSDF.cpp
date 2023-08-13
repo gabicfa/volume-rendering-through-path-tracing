cf3296259b2199a4825a8d76cfd151007a4f5779
Gabriela Almeida (s5432356) <s5432356@bournemouth.ac.uk>
2023-08-13 13:28:40 +0100

    Finisih BSDFs comment

--- src/bsdfs/LightEmittingBSDF.cpp
+++ src/bsdfs/LightEmittingBSDF.cpp
@@ -5,17 +5,19 @@ LightEmittingBSDF::LightEmittingBSDF(const Computation& _comp)
 
 LightEmittingBSDF::~LightEmittingBSDF() {}
 
+// For light-emitting surfaces, we don't scatter or generate samples as they are sources of light.
+// Hence, set sample direction and the radiance (L) to zero. Probability density function (pdf) is also set to zero.
 void LightEmittingBSDF::generateSample(const Computation& _comp, ngl::Vec4& sampleDirection, ngl::Vec3& L, float& pdf)
 {
-    // Emitting light doesn't really scatter rays, so we don't generate any samples
     sampleDirection = ngl::Vec4(0, 0, 0, 0);
     L = ngl::Vec3(0, 0, 0);
     pdf = 0;
 }
 
+// For light-emitting surfaces, there's no light to scatter. Hence, radiance (L) is returned as zero 
+// and probability density function (pdf) is also set to zero.
 void LightEmittingBSDF::evaluateSample(const Computation& _comp, const ngl::Vec4& sampleDirection, ngl::Vec3& L, float& pdf)
 {
-    // Emitting light doesn't really scatter rays, so we return L as zero and pdf as zero
     L = ngl::Vec3(0, 0, 0);
     pdf = 0;
 }
