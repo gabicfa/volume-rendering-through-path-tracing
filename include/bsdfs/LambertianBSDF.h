f9243540330aff1214fb9b10c8027baeeaa44850
Gabriela Almeida (s5432356) <s5432356@bournemouth.ac.uk>
2023-08-13 12:59:42 +0100

    MetalBSDF comment

--- include/bsdfs/LambertianBSDF.h
+++ include/bsdfs/LambertianBSDF.h
@@ -12,12 +12,7 @@ class LambertianBSDF : public BSDF
         LambertianBSDF(const Computation& _comp);
         ~LambertianBSDF();
 
-        // Evaluates the Lambertian BRDF for a given sample direction. Computes the reflected
-        // light intensity (L) and the associated probability density function (pdf) for the sample.
         void evaluateSample(const Computation& _comp, const ngl::Vec4& sampleDirection, ngl::Vec3& L, float &pdf) override;
-
-        // Generates a sample direction for the Lambertian BRDF and computes the reflected
-        // light intensity (L) and its associated probability density function (pdf).
         void generateSample(const Computation& _comp, ngl::Vec4& sampleDirection, ngl::Vec3& L, float& pdf) override;
 
     private:
